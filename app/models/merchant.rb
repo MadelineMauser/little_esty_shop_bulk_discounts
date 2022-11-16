09087e99a9e1dcb1b0b0c93a00965c84f4547570
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-16 09:30:32 -0700

    Add discounted_merchant_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -77,9 +77,10 @@ class Merchant < ApplicationRecord
   end
 
   def discounted_merchant_revenue(invoice)
+    create_invoice_item_discounts
     invoice_items.joins(:bulk_discounts)
     .where(invoice_id: invoice.id)
-    
+    .sum('(invoice_items.quantity * invoice_items.unit_price) * (bulk_discounts.percentage_discount / 100.0)')
   end
 end
 #self.invoice_items.joins(:bulk_discounts).select("invoice_items.*, max(SELECT bulk_discounts.percentage_discount WHERE invoice_items.quantity >= bulk_discounts.quantity_threshold) as applied_discount").
